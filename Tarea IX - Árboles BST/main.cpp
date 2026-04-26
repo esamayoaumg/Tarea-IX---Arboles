1d6f55e3588202792a9a6aab4bda7a7b60790f07
Elder Geovani Samayoa Esquivel <esamayoae1@miumg.edu.gt>
2026-04-25 20:19:20 -0600

    Add files via upload

---
+++ Tarea IX - Árboles BST/main.cpp
@@ -0,0 +1,145 @@
+#include <iostream>
+using namespace std;
+
+
+struct Nodo {
+    int dato;
+    Nodo* izquierdo;
+    Nodo* derecho;
+};
+
+Nodo* crearNodo(int valor) {
+    Nodo* nuevo = new Nodo();
+    nuevo->dato = valor;
+    nuevo->izquierdo = NULL;
+    nuevo->derecho = NULL;
+    return nuevo;
+}
+
+Nodo* insertar(Nodo* raiz, int valor) {
+    if (raiz == NULL) {
+        return crearNodo(valor);
+    }
+
+    if (valor < raiz->dato) {
+        raiz->izquierdo = insertar(raiz->izquierdo, valor);
+    } else {
+        raiz->derecho = insertar(raiz->derecho, valor);
+    }
+
+    return raiz;
+}
+
+
+void inOrden(Nodo* raiz) {
+    if (raiz != NULL) {
+        inOrden(raiz->izquierdo);
+        cout << raiz->dato << " ";
+        inOrden(raiz->derecho);
+    }
+}
+
+void preOrden(Nodo* raiz) {
+    if (raiz != NULL) {
+        cout << raiz->dato << " ";
+        preOrden(raiz->izquierdo);
+        preOrden(raiz->derecho);
+    }
+}
+
+void postOrden(Nodo* raiz) {
+    if (raiz != NULL) {
+        postOrden(raiz->izquierdo);
+        postOrden(raiz->derecho);
+        cout << raiz->dato << " ";
+    }
+}
+
+
+void mostrarMenu() {
+    cout << "Nombre: Elder Geovani Samayoa Esquivel" << endl;
+
+
+    cout << "\n--- MENU ---";
+    cout << "\n1. Insertar valor";
+    cout << "\n2. Mostrar InOrden";
+    cout << "\n3. Mostrar PreOrden";
+    cout << "\n4. Mostrar PostOrden";
+    cout << "\n5. Mostrar todos los recorridos";
+    cout << "\n6. Salir";
+    cout << "\nSeleccione una opcion: ";
+}
+
+
+int main() {
+    Nodo* raiz = NULL;
+    int opcion, valor;
+
+    do {
+        mostrarMenu();
+        cin >> opcion;
+
+        switch (opcion) {
+            case 1:
+                cout << "Ingrese valor: ";
+                cin >> valor;
+                raiz = insertar(raiz, valor);
+                cout << "Valor insertado correctamente.\n";
+                break;
+
+            case 2:
+                if (raiz == NULL) {
+                    cout << "El arbol esta vacio.\n";
+                } else {
+                    cout << "Recorrido InOrden: ";
+                    inOrden(raiz);
+                    cout << endl;
+                }
+                break;
+
+            case 3:
+                if (raiz == NULL) {
+                    cout << "El arbol esta vacio.\n";
+                } else {
+                    cout << "Recorrido PreOrden: ";
+                    preOrden(raiz);
+                    cout << endl;
+                }
+                break;
+
+            case 4:
+                if (raiz == NULL) {
+                    cout << "El arbol esta vacio.\n";
+                } else {
+                    cout << "Recorrido PostOrden: ";
+                    postOrden(raiz);
+                    cout << endl;
+                }
+                break;
+
+            case 5:
+                if (raiz == NULL) {
+                    cout << "El arbol esta vacio.\n";
+                } else {
+                    cout << "InOrden: ";
+                    inOrden(raiz);
+                    cout << "\nPreOrden: ";
+                    preOrden(raiz);
+                    cout << "\nPostOrden: ";
+                    postOrden(raiz);
+                    cout << endl;
+                }
+                break;
+
+            case 6:
+                cout << "Saliendo del programa...\n";
+                break;
+
+            default:
+                cout << "Opcion invalida, intente de nuevo.\n";
+        }
+
+    } while (opcion != 6);
+
+    return 0;
+}
---
+++ Tarea IX - Árboles BST/main.h
@@ -0,0 +1,13 @@
+//
+// Created by geo71 on 25/04/2026.
+//
+
+#ifndef TAREA_IX_ÁRBOLES_BST_MAIN_H
+#define TAREA_IX_ÁRBOLES_BST_MAIN_H
+
+
+class main {
+};
+
+
+#endif //TAREA_IX_ÁRBOLES_BST_MAIN_H
